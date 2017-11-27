61233e631f315131d20fb72d67de65dccb6c0f39
Robert Kennedy <robertbartholomewkennedy@gmail.com>
2017-11-27 17:38:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element){
-  var array = [1];
- return array.unshift("foo")
-
+ 
 }
 function destructivelyAddElementToBeginningOfArray(){
   
